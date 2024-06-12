a4ec4bc2b5241374ddbb01922653c602d4080a7e
Marius Schueller <mariuseschueller@gmail.com>
2024-06-11 22:20:29 -0500

    Unit 1.2 added

---
+++ Unit 1/Examples/PrimitiveDataTypes.java
@@ -0,0 +1,17 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        // Declaring and initializing variables of different primitive data types
+
+        // Integers (whole numbers) 32-bit signed integer
+        int myInt = 42;
+        System.out.println("Integer: " + myInt);
+
+        // Floating-point numbers (decimal numbers) 64-bit floating point
+        double myDouble = 3.14;
+        System.out.println("Double: " + myDouble);
+
+        // Boolean (true/false) bit
+        boolean myBoolean = true;
+        System.out.println("Boolean: " + myBoolean);
+    }
+}
---
+++ Unit 1/Exercises/WhichDataType.java
@@ -0,0 +1,41 @@
+public class WhichDataType {
+    public static void main(String[] args) {
+        // Intructions: Choose the appropriate data type for each scenario, assign an example
+        // value, and print it.
+
+        // Question 1: What data type would you use for storing a person's age?
+        // Your answer here:
+
+
+        // Question 2: What data type would you use for storing the price of a product?
+        // Your answer here:
+
+
+        // Question 3: What data type would you use for storing whether a light is on or off?
+        // Your answer here:
+
+
+        // Question 4: What data type would you use for storing the population of a city?
+        // Your answer here:
+
+
+        // Question 5: What data type would you use for storing the average temperature of a location?
+        // Your answer here:
+
+
+        // Question 6: What data type would you use for storing a person's name?
+        // Your answer here:
+
+
+        // Question 7: What data type would you use for storing the number of pages in a book?
+        // Your answer here:
+
+
+        // Question 8: What data type would you use for storing whether a student has passed an exam?
+        // Your answer here:
+
+        // Question 9: Use the final keyword to create a variable of your choosing. Print what do
+        // you think this does?
+        // Your answer here:
+    }
+}
