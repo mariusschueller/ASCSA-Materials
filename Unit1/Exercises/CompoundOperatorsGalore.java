ac4f0a65b741a707e84b81ef259acd0e1f62c7a4
Marius Schueller <mariuseschueller@gmail.com>
2024-06-12 18:50:44 -0500

    1.4

---
+++ Unit1/Exercises/CompoundOperatorsGalore.java
@@ -0,0 +1,57 @@
+public class CompoundOperatorsGalore {
+    public static void main(String[] args) {
+        // write out code to solve the following statements
+        // please create a variable set to whatever number
+        // make a guess for what you think will be the expected result
+        // print out the result
+
+        // Problem 1
+        // Result of x += 5 * 3:
+        // Expected value:
+        // Code:
+
+
+        // Problem 2
+        // Result of y -= 3 + 5:
+        // Expected value:
+        // Code:
+
+
+        // Problem 3
+        // Result of z *= 2 + 3:
+        // Expected value:
+        // Code:
+
+
+        // Problem 4
+        // Result of w /= 5:
+        // Expected value:
+        // Code:
+
+
+        // Problem 5
+        // Result of a++:
+        // Expected value:
+        // Code:
+
+
+        // Problem 6
+        // Result of b--:
+        // Expected value:
+        // Code:
+
+
+        // Problem 7
+        // Result of c += ++c:
+        // Expected value:
+        // Code:
+
+
+        // Problem 8
+        // Result of d -= --d:
+        // Expected value:
+        // Code:
+
+
+    }
+}
